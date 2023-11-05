0109be7100a99fa5797a16bb4ef9e2afcddd447c
ChibuTheDev <chibueze.k.muoneke@gmail.com>
2023-11-05 06:11:16 -0300

    please deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import About from './components/About'
 import Project from './components/Project'
 import Contact from './components/Contact'
